e218631b26e25ebea2d66f5b43c62549a793a9db
Eric Shaw <ericshawmail@gmail.com>
2022-03-04 01:06:26 -0500

    form added

--- server/index.js
+++ server/index.js
@@ -1,22 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
-
-app.use('/posts', postRoutes)
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://crud:crud123@cluster0.lqpr8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.use('/posts', postRoutes);
+
+// const CONNECTION_URL = 'mongodb+srv://crud:crud123@cluster0.lqpr8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
   app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
 })
